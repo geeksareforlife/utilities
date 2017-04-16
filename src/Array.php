3de912afd06ca2d786334e55d811c6a123a70710
James Hayward <jhayward1980@gmail.com>
2017-04-16 21:28:37 +0100

    First Array functions

---
+++ src/Array.php
@@ -0,0 +1,30 @@
+<?php
+
+use GeeksAreForLife\Utilities;
+
+class Arrays
+{
+	public static function sanitiseArrayList($array, $fieldlist)
+	{
+		$newArray = [];
+
+		for ($i = 0; $i < count($array); $i++) {
+			$newArray[] = self::sanitiseArray($array[$i], $fieldlist);
+		}
+
+		return $newArray;
+	}
+
+	public static function sanitiseArray($array, $fieldlist)
+	{
+		$newArray = [];
+
+		foreach ($array as $name => $value) {
+			if (in_array($name, $fieldlist)) {
+				$newArray[$name] = $array[$name];
+			}
+		}
+
+		return $newArray;
+	}
+}

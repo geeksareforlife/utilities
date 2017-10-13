ac947233392f20dd95eaf6fe687fa4ae17679d92
James Hayward <jhayward1980@gmail.com>
2017-10-13 23:17:08 +0100

    Some docs

--- src/Arrays.php
+++ src/Arrays.php
@@ -2,8 +2,20 @@
 
 namespace GeeksAreForLife\Utilities;
 
+/**
+ * Arrays class
+ *
+ * Some functions to help work with arrays
+ */
 class Arrays
 {
+    /**
+     * Takes an array of associative arraya and returns a new array or arrays
+     * each with only the fields provided in the field list
+     * @param  array $array     The input array
+     * @param  array $fieldlist a list of fields to keep
+     * @return array            the output array
+     */
     public static function sanitiseArrayList($array, $fieldlist)
     {
         $newArray = [];
@@ -15,6 +27,13 @@ class Arrays
         return $newArray;
     }
 
+    /**
+     * Takes an associative array and returns a new array with only the
+     * fields provided in the field list
+     * @param  array $array     The input array
+     * @param  array $fieldlist a list of fields to keep
+     * @return array            the output array
+     */
     public static function sanitiseArray($array, $fieldlist)
     {
         $newArray = [];

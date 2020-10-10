1d304e6247967ec3884ace1c881fe8ebcfe8ea13
James Hayward <jhayward1980@gmail.com>
2020-10-10 11:42:48 +0100

    Test for the Strings Class

---
+++ tests/unit/StringsTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use GeeksAreForLife\Utilities\Strings;
+
+class StringsTest extends TestCase
+{
+    /** @test */
+    public function string_starts_with_str(): void
+    {
+        $inputString = "string";
+        $test = "str";
+
+        $this->assertTrue(Strings::startsWith($inputString, $test));
+    }
+
+    /** @test */
+    public function string_doesnt_start_with_xyz(): void
+    {
+        $inputString = "string";
+        $test = "xyz";
+
+        $this->assertFalse(Strings::startsWith($inputString, $test));
+    }
+
+    /** @test */
+    public function string_ends_with_ing(): void
+    {
+        $inputString = "string";
+        $test = "ing";
+
+        $this->assertTrue(Strings::endsWith($inputString, $test));
+    }
+
+    /** @test */
+    public function string_doesnt_end_with_xyz(): void
+    {
+        $inputString = "string";
+        $test = "xyz";
+
+        $this->assertFalse(Strings::endsWith($inputString, $test));
+    }
+
+    /** @test */
+    public function string_ends_with_nothing(): void
+    {
+        $inputString = "string";
+        $test = "";
+
+        $this->assertTrue(Strings::endsWith($inputString, $test));
+    }
+}
